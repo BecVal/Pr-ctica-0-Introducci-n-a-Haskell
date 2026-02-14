6883741983853017e7484819a848a9e53f7cd32e
BecVal <cesarbecval@ciencias.unam.mx>
2026-02-14 13:09:15 -0600

    Archivo base de Haskell creado.

---
+++ Practica_0.hs
@@ -0,0 +1,37 @@
+{-
+    Práctica 0 - Introducción a Haskell de Lógica Computacional
+    Profesor: Dr. Marco Vladimir Lemus Yañez
+    Ayudante: Dr. Fernando Cruz Pineda
+    Fecha: 14/02/2026 
+
+    Inetgrantes del equipo:
+    - Becerra Valencia César
+    - Cortez Nava José Luis
+    - Javier
+-}
+
+-- PARTE A: RECURSIÓN SOBRE NÚMEROS
+
+-- Ejercicio 1. Fibonacci
+
+-- Ejercicio 2. Módulo n
+
+-- Ejercicio 3. MCD
+
+-- PARTE B: RECURSIÓN SOBRE LISTAS
+
+-- Ejercicio 4. Reversa de lista
+
+-- Ejercicio 5. Máximo de elementos
+
+-- Ejercicio 6. Filtrar lista
+
+-- Ejercicio 7. Contar Ocurrencias
+
+-- PARTE C: TIPOS ALGÉBRAICOS
+
+-- Ejercicio 8. Definición de números naturales
+
+-- Ejercicio 9. Conversión a entero
+
+-- Ejercicio 10. Multiplicación sobre Nat
